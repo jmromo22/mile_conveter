5d38484a86788ea0f8a18f4b03134a6a9f275ca3
Jesus Romo <romojesus03@gmail.com>
2022-02-19 12:34:46 -0800

    Added Comments

--- miles_converter.py
+++ miles_converter.py
@@ -1,7 +1,7 @@
 print("How many kilometers did you cycle today?")
-kms = input()
-miles = float(kms)/1.60934
-miles = round(miles, 2)
+kms = input() #get user input
+miles = float(kms)/1.60934 #converting from string to floats then divide
+miles = round(miles, 2) #rounds the result
 print(f"Your {kms}km ride is equal to {miles} miles")
 
 
